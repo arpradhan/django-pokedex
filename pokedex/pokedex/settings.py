6b557a31db2420963b7205a2adb99b8980ea6552
Abhijit Pradhan <abhijit@architizer.com>
2018-04-16 18:59:02 -0500

    Rotate and remove secret key

--- pokedex/pokedex/settings.py
+++ pokedex/pokedex/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h%p%1o^3@1(zo1i37c8#6vcn)1#0abz8hm&c-ylqgtwzo9k5(r'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
